fe6837819228793949a0de98207cae319e3eeec3
Davi Neves <54684941+DaviNeves0@users.noreply.github.com>
2022-04-13 07:58:29 -0300

    Button design

--- src/components/ListUser.vue
+++ src/components/ListUser.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="container">
     <div class="container-table">
-      <table>
-        <tr class="table-header">
-          <th>Id:</th>
-          <th>Nome:</th>
-          <th>Email:</th>
-          <th>telefone:</th>
-        </tr>
-        <tr v-for="item in data" :key="item.id">
-          <td>{{ item.id }}</td>
-          <td>{{ item.name }}</td>
-          <td>{{ item.email }}</td>
-          <td>{{ item.cellphoneNumber }}</td>
-        </tr>
-      </table>
-      <div>
-        <button :disabled="currentPage === 0" @click="voltar">Voltar</button>
-        <button :disabled="data.length < 20" @click="avancar">Avançar</button>
+      <div v-if="show === false">
+        <p>Nenhum dado encontrado</p>
+      </div>
+      <div v-else>
+        <table>
+          <tr class="table-header">
+            <th>Id:</th>
+            <th>Nome:</th>
+            <th>Email:</th>
+            <th>telefone:</th>
+          </tr>
+          <tr v-for="item in data" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.email }}</td>
+            <td>{{ item.cellphoneNumber }}</td>
+          </tr>
+        </table>
+        <div>
+          <button :disabled="currentPage === 0" @click="voltar" class="btn-voltar">&lt; Voltar</button>
+          <button :disabled="data.length < 20" @click="avancar" class="btn-avancar">Avançar &gt;</button>
+        </div>
       </div>
     </div>
   </div>
@@ -32,6 +37,7 @@ export default {
     return {
       currentPage: 0,
       data: [],
+      show: false,
     };
   },
   methods: {
@@ -40,6 +46,11 @@ export default {
         .then((res) => {
           console.log(res.data);
           this.data = res.data;
+          if (res.data.length > 0) {
+            this.show = true;
+          } else {
+            this.show = false;
+          }
         })
         .catch((e) => {
           alert("Não foi possível carregar a base de dados");
@@ -57,11 +68,13 @@ export default {
   },
   mounted() {
     this.retrieveUsers();
+    console.log(this.show)
   },
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap");
 .container {
   display: flex;
   justify-content: center;
@@ -90,5 +103,30 @@ td {
   border-bottom: 2px solid #cfcfcf;
   padding-top: 20px;
   padding-bottom: 20px;
+}
+
+button{
+  background:#15ed48 ;
+  border-radius: 20px;
+  border: 1px solid #535252;
+  padding: 8px;
+  color: #535252;
+  font-family: "Outfit",sans-serif;
+  font-weight: bold;
+  margin-top: 20px;
+
+}
+
+button:disabled{
+  background: transparent;
+  padding: 8px;
+  border: 1px solid #535252;
+  color: #cfcfcf;
+  border-radius: 20px;
+  font-family: "Outfit",sans-serif;
+}
+
+.btn-avancar{
+  margin-left: 10px;
 }
 </style>
